84792d7d16ddd54ecb23674cf8524c4d6120509f
Jessica <ulmer@fh-aachen.de>
2022-03-28 10:27:34 +0200

    new build

--- Assets/Scripts/Communication/Connection.cs
+++ Assets/Scripts/Communication/Connection.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NativeWebSocket;
-//using Newtonsoft.Json;
-using MessagePack;
+using System.Text.RegularExpressions;
 
 public class Connection : MonoBehaviour
 {
@@ -98,30 +97,13 @@ public class Connection : MonoBehaviour
             init_received = true;
             SendWebSocketMessage("ACK-new_instructions");
         }
-        else if (message.Contains("version"))  //Set product version
+        else if (message.Contains("version"))  //Set product version {"version": "V3.3"}
         {
             //order_properties = JsonConvert.DeserializeObject<OrderProperties>(message);
-            //this.GetComponent<MessageHandler>().InitializeVersion(order_properties.version);
-            this.GetComponent<MessageHandler>().InitializeVersion("V3.3");
+            Regex rx = new Regex(@"version<(.*?)>");
+            string result = rx.Match(message).Groups[1].Value;
+            this.GetComponent<MessageHandler>().InitializeVersion(result);
             SendWebSocketMessage("ACK-version");
-
-            var mc = new MyClass
-            {
-                Age = 99,
-                FirstName = "hoge",
-                LastName = "huga",
-            };
-            // Call Serialize/Deserialize, that's all.
-            byte[] bytes = MessagePackSerializer.Serialize(mc);
-            MyClass mc2 = MessagePackSerializer.Deserialize<MyClass>(bytes);
-
-            // You can dump MessagePack binary blobs to human readable json.
-            // Using indexed keys (as opposed to string keys) will serialize to MessagePack arrays,
-            // hence property names are not available.
-            // [99,"hoge","huga"]
-            var json = MessagePackSerializer.ConvertToJson(bytes);
-            Debug.Log(json);
-
         }
         //else if (message.Contains("number_steps"))  //Set number of steps
         //{
@@ -245,24 +227,4 @@ public class Connection : MonoBehaviour
     {
         await websocket.Close();
     }
-}
-
-[MessagePackObject]
-public class MyClass
-{
-    // Key attributes take a serialization index (or string name)
-    // The values must be unique and versioning has to be considered as well.
-    // Keys are described in later sections in more detail.
-    [Key(0)]
-    public int Age { get; set; }
-
-    [Key(1)]
-    public string FirstName { get; set; }
-
-    [Key(2)]
-    public string LastName { get; set; }
-
-    // All fields or properties that should not be serialized must be annotated with [IgnoreMember].
-    [IgnoreMember]
-    public string FullName { get { return FirstName + LastName; } }
 }
